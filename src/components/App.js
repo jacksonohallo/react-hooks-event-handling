d879c8cd56afb69240cfed9bf6804e70dd63f39e
jacksonohallo <jacksonohallo@gmail.com>
2022-07-17 21:52:45 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -11,6 +11,7 @@ function App() {
       <Tickler />
       <hr />
 
+
       <MultiButton />
       <hr />
 
---
+++ src/components/Clickable.js
@@ -0,0 +1,14 @@
+// import React from "react";
+
+// function Clickable() {
+    
+//     function handleClick() {
+//      console.log("click")        
+// }
+
+
+// return <button onClick={handleClick}>click</button>
+
+// }
+    
+// export default Clickable;
